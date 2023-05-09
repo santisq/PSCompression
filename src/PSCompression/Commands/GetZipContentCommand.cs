6e05e72d03bfaaeb31d03453fdd7fe34fa2a80a8
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-05-08 23:47:06 -0300

    new cmdlets

--- src/PSCompression/Commands/GetZipContentCommand.cs
+++ src/PSCompression/Commands/GetZipContentCommand.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Management.Automation;
 
 namespace PSCompression;
@@ -6,6 +8,8 @@ namespace PSCompression;
 [Cmdlet(VerbsCommon.Get, "ZipContent", DefaultParameterSetName = "Raw")]
 public sealed class GetZipContentCommand : PSCmdlet
 {
+    private List<string> _content = new();
+
     [Parameter(Mandatory = true, ValueFromPipeline = true)]
     public ZipEntryFile[]? InputObject { get; set; }
 
@@ -26,6 +30,9 @@ public sealed class GetZipContentCommand : PSCmdlet
         {
             try
             {
+                using ZipEntryStream stream = entry.OpenRead();
+                using StreamReader reader = new(stream);
+
 
             }
             catch (PipelineStoppedException)
