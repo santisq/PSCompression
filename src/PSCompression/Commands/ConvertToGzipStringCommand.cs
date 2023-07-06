20ef6d91711a3be2b0e5d6cdb434ba4a40563b0d
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-07-05 23:29:02 -0300

    ConvertToGzipStringCommand.cs

--- src/PSCompression/Commands/ConvertToGzipStringCommand.cs
+++ src/PSCompression/Commands/ConvertToGzipStringCommand.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.IO.Compression;
 using System.Management.Automation;
 using System.Text;
@@ -5,15 +7,22 @@ using System.Text;
 namespace PSCompression;
 
 [Cmdlet(VerbsData.ConvertTo, "GzipString")]
-[OutputType(typeof(byte), ParameterSetName = new string[1] { "ByteSteream" })]
+[OutputType(typeof(byte), ParameterSetName = new string[1] { "ByteStream" })]
 [OutputType(typeof(string))]
 [Alias("gziptostring")]
-public sealed class ConvertToGzipStringCommand : PSCmdlet
+public sealed class ConvertToGzipStringCommand : PSCmdlet, IDisposable
 {
+    private StreamWriter? _writer;
+
+    private GZipStream? _gzip;
+
+    private readonly MemoryStream _outstream = new();
+
     [Parameter(Mandatory = true, ValueFromPipeline = true)]
     [AllowEmptyString]
     public string[] InputObject { get; set; } = null!;
 
+    [Parameter]
     [ArgumentCompleter(typeof(EncodingCompleter))]
     [EncodingTransformation]
     [ValidateNotNullOrEmpty]
@@ -28,4 +37,85 @@ public sealed class ConvertToGzipStringCommand : PSCmdlet
 
     [Parameter]
     public SwitchParameter NoNewLine { get; set; }
+
+    protected override void ProcessRecord()
+    {
+        try
+        {
+            _gzip ??= new(_outstream, CompressionLevel);
+            _writer ??= new(_gzip, Encoding);
+
+            if (NoNewLine.IsPresent)
+            {
+                Write(_writer, InputObject);
+                return;
+            }
+
+            WriteLines(_writer, InputObject);
+        }
+        catch (PipelineStoppedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            WriteError(new ErrorRecord(
+                e, "WriteError", ErrorCategory.NotSpecified, InputObject));
+        }
+    }
+
+    protected override void EndProcessing()
+    {
+        _writer?.Dispose();
+        _gzip?.Dispose();
+        _outstream?.Dispose();
+
+        try
+        {
+            if (_writer is null || _gzip is null || _outstream is null)
+            {
+                return;
+            }
+
+            if (AsByteStream.IsPresent)
+            {
+                WriteObject(_outstream.ToArray(), enumerateCollection: true);
+                return;
+            }
+
+            WriteObject(Convert.ToBase64String(_outstream.ToArray()));
+        }
+        catch (PipelineStoppedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            WriteError(new ErrorRecord(
+                e, "OutputError", ErrorCategory.NotSpecified, _outstream));
+        }
+    }
+
+    private void WriteLines(StreamWriter writer, string[] lines)
+    {
+        foreach (string line in lines)
+        {
+            writer.WriteLine(line);
+        }
+    }
+
+    private void Write(StreamWriter writer, string[] lines)
+    {
+        foreach (string line in lines)
+        {
+            writer.Write(line);
+        }
+    }
+
+    public void Dispose()
+    {
+        _writer?.Dispose();
+        _gzip?.Dispose();
+        _outstream?.Dispose();
+    }
 }
