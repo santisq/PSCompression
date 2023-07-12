1df91c42cf6a64aea5eaf2d33f4129e0538a0be5
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-07-12 18:57:06 -0300

    updates to expandgziparchive

--- src/PSCompression/Commands/ExpandGzipArchiveCommand.cs
+++ src/PSCompression/Commands/ExpandGzipArchiveCommand.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.IO.Compression;
 using System.Management.Automation;
 using System.Text;
 
@@ -18,12 +17,6 @@ namespace PSCompression;
 [Alias("gzipfromfile")]
 public sealed class ExpandGzipArchiveCommand : PSCmdlet
 {
-    private const byte GzipPreamble1 = 0x1f;
-
-    private const byte GzipPreamble2 = 0x8b;
-
-    private const byte GzipPreamble3 = 0x08;
-
     private bool _isLiteral;
 
     private FileStream? _destination;
@@ -142,7 +135,12 @@ public sealed class ExpandGzipArchiveCommand : PSCmdlet
 
             try
             {
-
+                GzipReaderOps.GetContent(
+                    path: path,
+                    isCoreCLR: PSVersionHelper.IsCoreCLR,
+                    raw: Raw.IsPresent,
+                    encoding: Encoding,
+                    cmdlet: this);
             }
             catch (PipelineStoppedException)
             {
@@ -153,62 +151,5 @@ public sealed class ExpandGzipArchiveCommand : PSCmdlet
                 WriteError(ExceptionHelpers.ZipOpenError(path, e));
             }
         }
-    }
-    private string ReadGzipFramework(string path)
-    {
-        int marker = 0;
-        int b;
-        using FileStream fs = File.OpenRead(path);
-        MemoryStream outmem = new();
-
-        while ((b = fs.ReadByte()) != -1)
-        {
-            if (marker == 0 && (byte)b == GzipPreamble1)
-            {
-                marker++;
-                continue;
-            }
-
-            if (marker == 1)
-            {
-                if ((byte)b == GzipPreamble2)
-                {
-                    marker++;
-                    continue;
-                }
-
-                marker = 0;
-            }
-
-            if (marker == 2)
-            {
-                marker = 0;
-
-                if ((byte)b != GzipPreamble3)
-                {
-                    AppendBytes(path, outmem, fs.Position - 3);
-                }
-            }
-        }
-
-        outmem.Seek(0, SeekOrigin.Begin);
-        using StreamReader reader = new(outmem);
-        return reader.ReadToEnd();
-    }
-
-    private void AppendBytes(string path, MemoryStream outmem, long pos)
-    {
-        using FileStream substream = File.OpenRead(path);
-        substream.Seek(pos, SeekOrigin.Begin);
-        using GZipStream gzip = new(substream, CompressionMode.Decompress);
-        gzip.CopyTo(outmem);
-    }
-
-    private string ReadGzipCore(string path)
-    {
-        using FileStream fs = File.OpenRead(path);
-        using GZipStream gzip = new(fs, CompressionMode.Decompress);
-        using StreamReader reader = new(gzip);
-        return reader.ReadToEnd();
     }
 }
