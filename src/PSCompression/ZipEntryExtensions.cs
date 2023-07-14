10d045ac786652ea8615245ca758e036300f6d75
Santiago Squarzon <squarzon.santiago@gmail.com>
2023-07-14 18:20:57 -0300

    some progress on compressziparchivecommand

---
+++ src/PSCompression/ZipEntryExtensions.cs
@@ -0,0 +1,51 @@
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+
+namespace PSCompression;
+
+public static class ZipEntryExtensions
+{
+    private static readonly Regex s_reNormalize = new(@"[\\/]+|(?<![\\/])$",
+        RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+    private static readonly Regex s_reEntryDir = new(@"[\\/]$",
+        RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+    private const string _pathChar = "/";
+
+    internal static string NormalizeEntryPath(this string path) =>
+        s_reNormalize.Replace(path, _pathChar).TrimStart('/');
+
+    internal static string NormalizeFileEntryPath(this string path) =>
+        NormalizeEntryPath(path).TrimEnd('/');
+
+    internal static bool IsDirectoryPath(this string path) =>
+        s_reEntryDir.IsMatch(path);
+
+    public static string NormalizePath(this string path) =>
+    s_reEntryDir.IsMatch(path) ? NormalizeEntryPath(path) :
+        NormalizeFileEntryPath(path);
+
+    internal static ZipArchiveEntry CreateEntryFromFile(
+        this ZipArchive zip,
+        string entry,
+        FileStream fileStream,
+        CompressionLevel compressionLevel)
+    {
+        if (entry.IsDirectoryPath())
+        {
+            return zip.CreateEntry(entry);
+        }
+
+        fileStream.Seek(0, SeekOrigin.Begin);
+        ZipArchiveEntry newentry = zip.CreateEntry(entry, compressionLevel);
+
+        using (Stream stream = newentry.Open())
+        {
+            fileStream.CopyTo(stream);
+        }
+
+        return newentry;
+    }
+}
